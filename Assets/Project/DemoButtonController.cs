10b45bac3d83eaa8ebf5d46760c6c2b30262e5a6
sallywuzhere <sallyslade@gmail.com>
2024-06-09 15:11:28 -0700

    adding public repo

---
+++ Assets/Project/DemoButtonController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoButtonController : MonoBehaviour
+{
+    [SerializeField] private Canvas parentCanvas;
+    [SerializeField] private BottomSheetController bottomSheetPrefab;
+
+    private BottomSheetController bottomSheetInstance;
+
+    public void OnUserPressButton()
+    {
+        if (bottomSheetInstance != null)
+        {
+            Destroy(bottomSheetInstance.gameObject);
+        }
+        else
+        {
+            bottomSheetInstance = Instantiate(bottomSheetPrefab, parentCanvas.transform);
+            bottomSheetInstance.parentCanvas = parentCanvas;
+        }
+    }
+}
